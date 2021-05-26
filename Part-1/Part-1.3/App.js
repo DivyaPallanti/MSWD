a354f3eb9879d905e3013e304636082b44fd1841
DivyaPallanti <73708106+DivyaPallanti@users.noreply.github.com>
2021-05-26 22:24:51 +0530

    Add files via upload

---
+++ Part-1/Part-1.3/App.js
@@ -0,0 +1,27 @@
+const course = 'Half Stack application development'
+  const part1 = 
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = 
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = 
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const total = 
+  {
+    name: 'Total number of exercises'
+  
+  }
+
+       console.log(course)
+       console.log(part1.name , part1.exercises)
+       console.log(part2.name , part2.exercises)
+       console.log(part3.name , part3.exercises)
+       console.log(total.name , part1.exercises + part2.exercises + part3.exercises)
