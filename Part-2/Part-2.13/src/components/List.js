0be1c1b32b0fc8b4c84ca077aaa851077f1a02af
DivyaPallanti <73708106+DivyaPallanti@users.noreply.github.com>
2021-05-31 00:20:59 +0530

    Add files via upload

--- Part-2/Part-2.13/src/components/List.js
+++ Part-2/Part-2.13/src/components/List.js
@@ -1,1 +1,38 @@
+import React from 'react'
+import CountryInfo from './CountryInfo'
+
+const List = ({ countries, newfilter,showC }) => {
+    const filtered = countries.filter(country =>
+      country.name.toUpperCase().includes(newfilter.toUpperCase())
+    );
+    const listLength = filtered.length;
+    console.log(filtered);
+    if(newfilter===''){
+          return <div />
+    }
+    else{
+
+     if (listLength >= 10) {
+      return <div>Too many matches, specify another letter(s)</div>;
+    }
+  
+    else if (listLength > 1 && listLength < 10) {
+      return filtered.map(country => (
+        <div key={country.name}>
+          {country.name}<button value={country.name} onClick={showC}>show</button>
+          
+        </div>
+      ));
+    } 
+    else {
+      return (
+        <div>
+          <CountryInfo country={filtered[0]} />
+        </div>
+      );
+    }
+}
+  };
+export default List
+
 
